b844296e05bef31077347b5168fdeaddbeb4c272
GregoryHeard <heard.gr@gmail.com>
2025-07-18 17:21:32 +0100

    D: Finalised reporting structure for main

--- main.py
+++ main.py
@@ -2,23 +2,34 @@ from stats import word_count
 from stats import character_count
 from stats import sort_dict
 
-# Main function returns word count in document
-def main():
-    file_path = "books/frankenstein.txt"
-    text = get_book_text(file_path)
-    num_words = word_count(text)
-    num_characters = character_count(text)
-    print(f"{num_words} words found in the document")
-    print(num_characters)
-
 # Turns a file.txt into a string
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
+# Main function returns report including total word count and individual character counts
+def main():
+    file_path = "books/frankenstein.txt"
+    text = get_book_text(file_path)
+    num_words = word_count(text)
+    num_characters = character_count(text)
+    report_counts = sort_dict(num_characters)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    index = 0
+    for n in report_counts:
+        char = report_counts[index]["char"]
+        num = report_counts[index]["num"]
+        index += 1
+        if char.isalpha() == True:
+            print(f"{char}: {num}")
+    print("============= END ===============")
+        
 
-       
+      
 main()
-
 
 
--- stats.py
+++ stats.py
@@ -1,16 +1,14 @@
-#Returns number of words in string
+#Returns number of words in text
 def word_count(text):
     words = text.split()
     return len(words)
-
-message = "Hello world"
 
 #Counts the number of occurences of each character in the text
 def character_count(text):
     lowercase_text = text.lower() #makes text lower case
     unique_characters = set(lowercase_text) #creates a set of unique characters found in the text
     dictionary = dict.fromkeys(unique_characters) #creates a dictionary with each unique character as a key
-    all_characters = list(lowercase_text)
+    all_characters = list(lowercase_text) #makes a list from the lowercase text
     for key in dictionary: #for every character in the text, replace value in dictionary with count of character in text
         count = 0
         for character in all_characters:
@@ -19,11 +17,11 @@ def character_count(text):
             dictionary[key] = count
     return dictionary
 
-#test_dict = {"a" : 1 , "b" : 2 , "c" : 3}
-
+#To be used by .sort() to sort the items in a list from highest to lowest value
 def sort_on(items):
     return items["num"]
 
+#Turns the dictionary into a list of dictionaries sorted from highest to lowest value
 def sort_dict(unsorted_dict):
     list = []
     for item in unsorted_dict:

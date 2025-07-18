a12f1daa43d9180a3d26a30bf779feea7550f578
GregoryHeard <heard.gr@gmail.com>
2025-07-18 14:59:14 +0100

    B: Added character count function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from stats import get_word_count
+from stats import word_count
+from stats import character_count
 
 # Main function returns word count in document
 def main():
     file_path = "books/frankenstein.txt"
     text = get_book_text(file_path)
-    num_words = get_word_count(text)
+    num_words = word_count(text)
+    num_characters = character_count(text)
     print(f"{num_words} words found in the document")
+    print(num_characters)
 
 # Turns a file.txt into a string
 def get_book_text(file_path):
--- stats.py
+++ stats.py
@@ -1,6 +1,20 @@
 #Returns number of words in string
-def get_word_count(text):
+def word_count(text):
     words = text.split()
     return len(words)
 
+message = "Hello world"
+
 #Counts the number of occurences of each character in the text
+def character_count(text):
+    lowercase_text = text.lower() #makes text lower case
+    unique_characters = set(lowercase_text) #creates a set of unique characters found in the text
+    dictionary = dict.fromkeys(unique_characters) #creates a dictionary with each unique character as a key
+    all_characters = list(lowercase_text)
+    for key in dictionary: #for every character in the text, replace value in dictionary with count of character in text
+        count = 0
+        for character in all_characters:
+            if character == key:
+                count += 1
+            dictionary[key] = count
+    return dictionary
